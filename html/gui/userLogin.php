57e6a7778fc3e8cf2360247f7e49138e2b5aeacc
Joakim Gustavsson <Joakim.Gustavsson@infotiv.se>
2018-09-17 15:47:55 +0200

    register user and a basic login function.

---
+++ html/gui/userLogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<form NAME ="FORM" ACTION="../includes/login.inc.php" method="POST">
+				<input type="email" id="email" required="required" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" placeholder="E-mail">
+				<input type="password" id="password" required="required" name="pass" pattern=".{6,}" title="Six or more characters" placeholder="Password">
+				<br>
+				<button type="submit" name="submit">Login</button>
+				<button onclick="location.href='index.html'" class="selectBtn">Home</button>
+
+			</form>
+</body>
+</html>
---
+++ html/includes/registration.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+	include_once 'dbh.inc.php';
+
+	$firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
+	$lastName = mysqli_real_escape_string($conn, $_POST['lastName']);
+	$phone = mysqli_real_escape_string($conn, $_POST['phone']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$pass = mysqli_real_escape_string($conn, $_POST['pass']);
+
+	$hashedPass = password_hash($pass, PASSWORD_DEFAULT);
+	//Inserting user into the database
+
+	$sql2 = "SELECT * FROM users WHERE user_email='".$email."';";
+	$validEmail = $conn->query($sql2);
+	$sql3 = "SELECT * FROM users WHERE user_phone='".$phone."';";
+	$validPhone = $conn->query($sql3);
+	
+	if ($validEmail->num_rows > 0) {
+		echo "That email is already being used.";
+		exit();
+
+	}else {
+		if ($validPhone->num_rows > 0) {
+		echo "That Phone number is already being used.";
+		exit();
+		} else {
+		$sql = "INSERT INTO users (user_first, user_last, user_email, user_phone, user_pass) VALUES('".$firstName."','".$lastName."','".$email."', '".$phone."', '".$hashedPass."');";
+	 	mysqli_query($conn, $sql);
+   		echo "User account created for: " . $firstName . " " . $lastName ;
+		}
+	}
+	
+
+
+
+
