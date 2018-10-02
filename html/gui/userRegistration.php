75b3c3a131af9bbebd79815f568623e1201793b0
Joakim Gustavsson <Joakim.Gustavsson@infotiv.se>
2018-10-02 13:43:43 +0200

    updated documentation, fixed restrictions and error messages on create user

--- html/gui/userRegistration.php
+++ html/gui/userRegistration.php
@@ -13,7 +13,13 @@
 </head>	
 <header>
 	<!--Imports the header from getHeader.inc.php by including it with php-->
-	<?php include_once '../includes/getHeader.inc.php'; ?>
+	<?php include_once '../includes/getHeader.inc.php'; 
+	//checks if user is logged in, if NOT then return to index.
+	if(isset($_SESSION['u_id'])) {
+	header('Location: index.php');
+	exit;
+	}
+	?>
 </header>
 <body>
 
@@ -23,47 +29,38 @@
 		<div id="aboutHeading">
 			<h1 id="questionText">Create a new user</h1>
 		</div>	
-			<form onSubmit="return validate();" NAME ="FORM" ACTION="../includes/registration.inc.php" method="POST">		
+			<form NAME ="FORM" ACTION="../includes/registration.inc.php" method="POST">		
 				<div id="mainText">
 					
-						<input class="bigInputFields" type="text" required="required" name="firstName" placeholder="First name"><br>
-						<input class="bigInputFields" type="text" required="required" name="lastName" placeholder="Last name"><br>
-						<input class="bigInputFields" type="phone" required="required" name="phone" pattern="+[0-9]" placeholder="Phonenumber"><br>
-						<input class="bigInputFields" type="email" id="emailCreate" required="required" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,3}$" placeholder="E-mail">
-						<input class="bigInputFields" type="email" id="confirmEmail" required="required" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,3}$" placeholder="Confirm E-mail"><br>
+						<input class="bigInputFields" type="text" required="required" name="firstName" pattern="[a-zA-Z]{2,30}" title="only characters a-Z allowed" placeholder="First name" value="<?php echo $_SESSION['firstNamePH'];?>"><br>
+						<input class="bigInputFields" type="text" required="required" name="lastName" pattern="[a-zA-Z]{2,30}" title="only characters a-Z allowed" placeholder="Last name" value="<?php echo $_SESSION['lastNamePH'];?>"><br>
+						<input class="bigInputFields" type="phone" required="required" name="phone" pattern="[0-9]{10,13}" title="only numbers, 10-13characters, 00 for +." placeholder="Phonenumber" value="<?php echo $_SESSION['phonePH'];?>"><br>
+						<input class="bigInputFields" type="email" id="emailCreate" required="required" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,3}$" title="Characters@email.boi" placeholder="E-mail" value="<?php echo $_SESSION['emailPH'];?>">
+						<input class="bigInputFields" type="email" id="confirmEmail" required="required" name="emailCon" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,3}$" title="Characters@email.boi" placeholder="Confirm E-mail" value="<?php echo $_SESSION['emailPH'];?>"><br>
 						<input class="bigInputFields" type="password" id="passwordCreate" required="required" name="pass" pattern=".{6,}" title="Six or more characters" placeholder="Password">
-						<input class="bigInputFields" type="password" id="confirmPassword" required="required" name="passConfirm" pattern=".{6,}" title="Six or more characters" placeholder="Confirm Password"><br><br>		
+						<input class="bigInputFields" type="password" id="confirmPassword" required="required" name="passCon" pattern=".{6,}" title="Six or more characters" placeholder="Confirm Password"><br><br>		
 						<?php 
 							if(isset($_SESSION['errorCreate'])) {
 								?> <label id="signInError"><?php echo $_SESSION['errorCreate']; ?> </label> <?php
 								unset($_SESSION['errorCreate']);
 							}
+							if (isset($_POST['submit'])) { 
+ 								$_SESSION['firstNamePH'] = $_POST['firstName'];
+ 								$_SESSION['lastNamePH'] = $_POST['lastName'];
+ 								$_SESSION['phonePH'] = $_POST['phone'];
+					 			$_SESSION['emailPH'] = $_POST['email'];
+ 							} 
+
 						 ?>
 				</div>
 				<div id="versionNr">
+					<button class="bigButton" type="button" onclick="location.href='/infotivCarRental/html/gui/index.php'">Cancel</button>
 					<button class="bigButton" type="submit" name="submit">Create</button>
-					<button class="bigButton" onclick="location.href='/infotivCarRental/html/gui/index.php'">Cancel</button>	
+					
 				</div>
 			</form>
 	</div>
 	<div id="rightpane"></div>
 	</div>
 </body>
-	<script>
-        function validate(){
-            var a = document.getElementById("passwordCreate").value;
-            var b = document.getElementById("confirmPassword").value;
-            if (a!=b) {
-               alert("Passwords doesn't match");
-               return false;
-            } else {
-            	var c = document.getElementById("emailCreate").value;
-            	var d = document.getElementById("confirmEmail").value;
-            if (c!=d) {
-               alert("Email doesn't match");
-               return false;
-            }
-            }
-        }
-     </script> 
 </html>
